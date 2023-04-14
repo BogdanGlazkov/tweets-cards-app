3950393aceff95a09662469e51776e50725ee0e6
BogdanGlazkov <bglazkov@i.ua>
2023-04-15 00:28:10 +0300

    Add Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,17 @@
+import Topcard from "../../assets/images/carddecor.png";
+import s from "./Card.module.css";
+
 export default function Card() {
-  return <div className="Card">Card</div>;
+  return (
+    <div className={s.card}>
+      <img src={Topcard} alt="Card decoration" width={380} />
+      <div className={s.cardBottom}>
+        <p className={s.cardText}>777 tweets</p>
+        <p className={s.cardText}>100,500 Followers</p>
+        <button className={s.cardBtn} type="button">
+          <span className={s.cardBtnText}>Follow</span>
+        </button>
+      </div>
+    </div>
+  );
 }
