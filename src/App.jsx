3d707b3cb4a246bbf3ae09f17355b2ab1455d69b
BogdanGlazkov <bglazkov@i.ua>
2023-04-18 15:13:43 +0300

    Add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage.jsx";
 import TweetsPage from "./pages/Tweets/TweetsPage.jsx";
 import Layout from "./components/Layout/Layout";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/tweets-cards-app">
+    <HashRouter hashType="slash">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
@@ -14,6 +14,6 @@ export default function App() {
         </Route>
         <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
