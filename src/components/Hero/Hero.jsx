a56284687a57e6ee2082c17efe303ce355edddfc
BogdanGlazkov <bglazkov@i.ua>
2023-04-20 23:46:41 +0300

    Add lang buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,42 @@
-import heroPicSmall from "../../assets/images/friends-sm.jpg";
-import heroPicMiddle from "../../assets/images/friends-md.jpg";
-import heroPicLarge from "../../assets/images/friends-lg.jpg";
+import { useTranslation, Trans } from "react-i18next";
+import heroPic from "../../assets/images/friends.jpg";
 import Navigation from "../Navigation/Navigation";
 import s from "./Hero.module.css";
 
+const lngs = {
+  en: { nativeName: "English" },
+  ua: { nativeName: "Українська" },
+  ru: { nativeName: "Русский" },
+};
+
 export default function Hero() {
+  const { i18n } = useTranslation();
+
   return (
     <div className={s.hero}>
-      <h1 className={s.heroTitle}>Welcome to Tweets Application!</h1>
-      <h2 className={s.heroSubtitle}>You can find mates and friends here</h2>
-      <p className={s.heroText}>Subscribe and click the Tweets link below</p>
+      <div className={s.lng}>
+        {Object.keys(lngs).map((lng) => (
+          <button
+            type="submit"
+            key={lng}
+            onClick={() => i18n.changeLanguage(lng)}
+            disabled={i18n.resolvedLanguage === lng}
+          >
+            {lngs[lng].nativeName}
+          </button>
+        ))}
+      </div>
+      <h1 className={s.heroTitle}>
+        <Trans i18nKey="welcome">Welcome to Tweets Application!</Trans>
+      </h1>
+      <h2 className={s.heroSubtitle}>
+        <Trans i18nKey="find">You can find mates and friends here</Trans>
+      </h2>
+      <p className={s.heroText}>
+        <Trans i18nKey="click">Subscribe and click the Tweets link below</Trans>
+      </p>
       <div className={s.heroImgWrapper}>
-        <img
-          className={s.heroImg}
-          srcSet={`${heroPicLarge} 270w,
-          			${heroPicMiddle} 354w,
-          			${heroPicSmall} 460w,
-          			`}
-          sizes="(min-width: 1200px) 270px, (min-width: 768px) 354px, 100vw"
-          src={heroPicSmall}
-          alt="Friends"
-        />
+        <img className={s.heroImg} src={heroPic} alt="Friends" />
         <Navigation />
       </div>
     </div>
